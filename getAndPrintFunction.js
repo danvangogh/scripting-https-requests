6978bd386d53aae57bcb449f57d24c01731ad72a
danvangogh <djameswhite@yahoo.com>
2018-10-29 18:20:03 +0000

    created project folder and getAndPrintFunction file

---
+++ getAndPrintFunction.js
@@ -0,0 +1,12 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+console.log(requestOptions + '/n')
+
+}
